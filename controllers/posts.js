fb49ddc1091834a7a6e2c581f6ac99b64015833e
Walt Lungan <waltlungan@gmail.com>
2022-09-20 18:36:40 -0700

    need to tie in userID with commentID

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,6 +1,7 @@
 const cloudinary = require("../middleware/cloudinary");
 const Post = require("../models/Post");
 const Comment = require("../models/Comment"); //adding the requirement of comment model
+const User = require("../models/User");
 
 module.exports = {
   getProfile: async (req, res) => {
@@ -22,9 +23,11 @@ module.exports = {
   getPost: async (req, res) => {
     try {
       const post = await Post.findById(req.params.id);
+     // const user = await User.findById(req.params.id); //this is appearing as null. need to find solution
       const comments = await Comment.find({post: req.params.id}).sort({ createdAt: "desc" }).lean();
    // const comments = await Comment.find(req.params.id).sort({ createdAt: "desc" }).lean();
       res.render("post.ejs", { post: post, user: req.user, comments: comments });
+      console.log(user)
     } catch (err) {
       console.log(err);
     }
