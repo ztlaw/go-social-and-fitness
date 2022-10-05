6adec16eba867d83bb6d7297a9239037c9fd3b7b
ztlaw <waltlungan@gmail.com>
2022-10-04 23:14:44 -0700

    added styles, logos, landing feed

--- middleware/cloudinary.js
+++ middleware/cloudinary.js
@@ -3,9 +3,12 @@ const cloudinary = require("cloudinary").v2;
 require("dotenv").config({ path: "./config/.env" });
 
 cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.API_KEY,
-  api_secret: process.env.API_SECRET
+  // cloud_name: process.env.CLOUD_NAME,
+  // api_key: process.env.API_KEY,
+  // api_secret: process.env.API_SECRET
+  cloud_name: 'to-you-web-development',
+  api_key: '285412165377551',
+  api_secret: '04bm-g_GAYLEZJpawtpVkZu7XNs'
 });
 
 module.exports = cloudinary;
